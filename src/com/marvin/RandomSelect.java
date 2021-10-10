fd6b1c713d90996b24be42adfb0e0a51202766f9
goodmarvin <1120817774@qq.com>
2021-10-10 22:22:56 +0800

    load balance

---
+++ src/com/marvin/RandomSelect.java
@@ -0,0 +1,12 @@
+package com.itlaoge;
+
+import java.util.Random;
+
+//随机
+public class RandomSelect {
+    public static String getServer() {
+        Random random = new Random();
+        int rand = random.nextInt(ServerIps.LIST.size());
+        return ServerIps.LIST.get(rand);
+    }
+}
